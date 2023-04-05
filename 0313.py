acc72e4bfe94337c65da980640c9128418f32ea5
justin1112-jimin <55744033+justin1112-jimin@users.noreply.github.com>
2023-04-05 21:08:51 +0900

    Add files via upload

---
+++ 0313.py
@@ -0,0 +1,25 @@
+# print('(100>20) and (30 == 30) : ', (100>20) and (30 == 30))
+# print('(100>20) or (30 != 30) : ', (100>20) or (30 != 30))
+# print('not (30 != 30) : ', not (30 != 30))
+
+# print("안녕하세요? 여러분")
+# print("저는 차이썬을 무척 싫어합니다.")
+# print()
+
+
+
+# eng = int(input())
+# kor = int(input())
+# mat = int(input())
+
+# sum = eng + kor + mat
+# avg = sum / 3
+
+# print("합계 : ",sum)
+# print("평균 : ",avg)
+
+
+
+# num = 20233159
+
+# print()
